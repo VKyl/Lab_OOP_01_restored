afc29c905f3425e9c51366eedf19ffa7ef7b86cd
VKyl <viktor.kyliukh@gmail.com>
2025-03-06 00:47:52 +0200

    better visualisation

--- Kyliukh_OOP_01/main.cpp
+++ Kyliukh_OOP_01/main.cpp
@@ -5,8 +5,12 @@ int main()
 {
 	Point p(1, 1), p2(1, 2), p3(2, 5);
 	Triangle t(p, p2, p3);
-	std::cout << t.medianAB() << std::endl;
-	t.c(2, 3);
-	std::cout << t.medianAB() << std::endl;
+	std::cout << "Median on AB " << t.medianAB() << std::endl;
+	std::cout << "Median on BC " << t.medianBC() << std::endl;
+	std::cout << "Median on AC " << t.medianAC() << std::endl;
+	t = { {1,3}, {2, 3}, {4, 5} };
+	std::cout << "Median on AB " << t.medianAB() << std::endl;
+	std::cout << "Median on BC " << t.medianBC() << std::endl;
+	std::cout << "Median on AC " << t.medianAC() << std::endl;
 	return 0;
 }
